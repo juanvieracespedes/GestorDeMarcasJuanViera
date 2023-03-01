4d1f95d6a1e577e67fe585ae3b07fb9b5e3dd152
Juan Manuel Viera <juanvieracespedes@gmail.com>
2023-03-01 18:17:30 -0300

    ahora el admin agrega usuarios que son utilizables para el login

--- login.js
+++ login.js
@@ -8,19 +8,25 @@ document.querySelector('form').addEventListener('submit', function(event) {
   
   // Comprueba si el usuario y la contraseña son válidos
   if (isValidLogin(username, password)) {
-    swal({
-      icon: "success",
-    });;
-    // Aquí puedes redirigir al usuario a otra página
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Sesión iniciada correctamente',
+      showConfirmButton: false,
+      timer: 1500
+    });
+
+    window.location.href = "index.html";
   } else {
-    swal({
-      title: "Error",
-      text: "Usuario o/y contraseña incorrectos",
-      icon: "error",
-    });;
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Contraseña y/o usuario incorrecta/s!',
+      footer: '<a href="">olvidaste tu contraseña?</a>'
+    });
   }
 ;
-  })
+  });
 
   function isValidLogin(username, password) {
     // Accede a los usuarios almacenados en el localStorage
